c586e38fe3a2f091b8acbb8d5d15601c3fbf49af
Raulleao <raulleaochagas@gmail.com>
2022-04-12 22:25:25 -0400

    tacando as senhas no git

---
+++ geraQuebra.py
@@ -0,0 +1,45 @@
+from random import random, choice
+import string
+import time
+# usuário pode digitar a sua própria senhaUsuario ou escolher a que o programa sugerir
+
+tamanho = 10
+valores = string.ascii_letters  # todas as letras mas e min
+valores += string.digits  # numeros de 0 a 9
+valores += string.punctuation  # caracteres especiais
+
+
+def apresentacao(boasVindas):  # Programa irá apresentar a mensagem de Boas Vindas
+    print("\033[34m="*30)
+    print(boasVindas)
+    print("\033[34m=\033[m"*30)
+
+
+def gerarSenhas(valores):
+    global senhaGerada
+    senhaGerada = ""
+    for cont in range(tamanho):
+        senhaGerada += choice(valores)
+    len(senhaGerada)
+
+
+def quebrarSenha(valores):
+    global senhaEncontrada
+    senhaEncontrada = ""
+    while(len(senhaEncontrada) != tamanho):
+        caracteresEncontrados = len(senhaEncontrada)
+        caracter = choice(valores)
+        if (caracter == senhaGerada[caracteresEncontrados]):
+            senhaEncontrada += caracter
+            print(senhaEncontrada)
+            time.sleep(1)
+    print(f"\033[31mSENHA GERADA:\n{senhaGerada}")
+    print(f"\033[34mSENHA HACKEADA:\n{senhaEncontrada}")
+
+
+apresentacao("===== Seja Bem-vindo(a) ======")
+
+gerarSenhas(valores)
+print()
+quebrarSenha(valores)
+print()
---
+++ gerarSenha.py
@@ -0,0 +1,28 @@
+import numbers
+import random
+import string
+
+
+def password_generator(len_pass=8):
+    ascii_options = string.ascii_letters
+    numbers_options = string.digits
+    punt_options = string.punctuation
+
+    somaTudo = ascii_options+numbers_options+punt_options
+
+    password_user = ""
+
+    for cont in range(0, len_pass):
+        digit = random.choice(somaTudo)
+        password_user += digit
+    return password_user
+
+
+choice_user = input("Quantos digitos na senha: ")
+if choice_user.isdigit():
+    choice_user = int(choice_user)
+else:
+    print("Entrada Inválida")
+    quit()
+response = password_generator(choice_user)
+print(f"Senha gerada:\n{response}")
